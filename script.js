4ac96f0297b32569ab54f1e352a60967606f470f
imran <87297004+imranshaikh9930@users.noreply.github.com>
2023-10-20 16:27:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 //your JS code here. If required.
 
 document.addEventListener("DOMContentLoaded", function() {
-    const info = document.createElement("div");
-    
-    info.innerText = "DOM load success";
-
-    document.body.innerHTML = ''; 
-    document.body.appendChild(info);
+    document.body.innerHTML = "DOM load success";
 });
